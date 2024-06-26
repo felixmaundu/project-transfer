181f7214a6725c8004e303e9436073388510cdae
Felix Maundu <maundu@kenyare.co.ke>
2024-06-26 11:58:09 +0300

    first commit

---
+++ src/lib/context.ts
@@ -0,0 +1,45 @@
+import { Pinecone } from "@pinecone-database/pinecone";
+import { convertToAscii } from "./utils";
+import { getEmbeddings } from "./embeddings";
+
+export async function getMatchesFromEmbeddings(
+  embeddings: number[],
+  fileKey: string
+) {
+  try {// 1.0.1
+    const client = new Pinecone({
+      apiKey: process.env.PINECONE_API_KEY!,//"d7aa29a2-4aa7-4d4d-99a4-9f4ab3614a77",//
+      environment: process.env.PINECONE_ENVIRONMENT!,
+    });
+    const pineconeIndex = await client.index("kre-policies-project-vector-index");
+    const namespace = pineconeIndex.namespace(convertToAscii(fileKey));
+    const queryResult = await namespace.query({
+      topK: 5,
+      vector: embeddings,
+      includeMetadata: true,
+      
+    });
+    return queryResult.matches || [];
+  } catch (error) {
+    console.log("error querying embeddings", error);
+    throw error;
+  }
+}
+
+export async function getContext(query: string, fileKey: string) {
+  const queryEmbeddings = await getEmbeddings(query);
+  const matches = await getMatchesFromEmbeddings(queryEmbeddings, fileKey);
+
+  const qualifyingDocs = matches.filter(
+    (match) => match.score && match.score > 0.7
+  );
+
+  type Metadata = {
+    text: string;
+    pageNumber: number;
+  };
+
+  let docs = qualifyingDocs.map((match) => (match.metadata as Metadata).text);
+  // 5 vectors
+  return docs.join("\n").substring(0, 3000);
+}
